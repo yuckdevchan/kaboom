82ae23d129f4181069d0ea94a503f0ea57502ac9
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2024-03-13 07:13:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 import os, pathlib, subprocess
 import win32com.client as win32
+from pathlib import Path
+
+def current_user() -> str:
+    return os.getlogin()
 
 def launch_program(program):
-        subprocess.check_call([f"{program}"])
+        print(program)
+        subprocess.Popen(program)
 
-def list_programs():
-    start_menu_list = os.listdir("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs")
-    program_list = []
-    for item in start_menu_list:
-        if not item.endswith(".lnk"):
-            start_menu_list.remove(item)
-            program_list.append(item)
-    for folder in start_menu_list:
-        for item in os.listdir(f"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\{folder}"):
-            if item.endswith(".lnk"):
-                program_list.append(f"{folder}\\{item}")
-    return 
+def list_programs(directories: list) -> list:
+    lnk_files = []
+    for directory in directories:
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.endswith(".lnk"):
+                    relative_path = Path(root).relative_to(directory)
+                    lnk_files.append(str(Path(relative_path) / file))
+    return lnk_files
 
-def run_shortcut(shortcut):
+def run_shortcut(shortcut: str):
     shell = win32.Dispatch("WScript.Shell")
     shortcut = shell.CreateShortCut(shortcut)
     launch_program(shortcut.Targetpath)
 
 def cli():
-    program_list = list_programs()
+    program_list = list_programs(directories)
     for i in range(0, len(program_list)):
-        print(f"{i + 1}. {program_list[i]}")
+        if program_list[i] == "Immersive Control Panel.lnk":
+            program_display_name = "Control Panel"
+        else:
+            program_display_name = program_list[i].split(".lnk")[0]
+            program_display_name = program_display_name.split("\\")[-1]
+        print(f"{i + 1}. {program_display_name}")
     chosen_program = input("Enter program index: ")
     run_shortcut(f"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\{program_list[int(chosen_program) - 1]}")
+
+directories = [
+    'C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs',
+    f'C:\\Users\\{current_user()}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs'
+]
 
 cli()
